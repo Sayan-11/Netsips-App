4e71ead74c53cf8de3ba58aab273d154656c14f6
Aryan <aryan.arora180@gmail.com>
2020-04-30 20:49:31 +0530

    Using navigation component now

--- app/src/main/java/in/netsips/netsipsapp/MainActivity.kt
+++ app/src/main/java/in/netsips/netsipsapp/MainActivity.kt
@@ -1,12 +1,11 @@
 package `in`.netsips.netsipsapp
 
 import `in`.netsips.netsipsapp.databinding.ActivityMainBinding
-import `in`.netsips.netsipsapp.ui.NotificationsFragment
-import `in`.netsips.netsipsapp.ui.archive.ArchiveFragment
-import `in`.netsips.netsipsapp.ui.home.HomeFragment
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
+import androidx.navigation.findNavController
+import androidx.navigation.ui.setupWithNavController
 
 class MainActivity : AppCompatActivity() {
 
@@ -15,44 +14,7 @@ class MainActivity : AppCompatActivity() {
         val binding: ActivityMainBinding =
             DataBindingUtil.setContentView(this, R.layout.activity_main)
 
-        supportFragmentManager.beginTransaction().replace(
-            R.id.main_activity_frame,
-            HomeFragment()
-        )
-            .commit()
-
-        binding.bottomNavigation.setOnNavigationItemSelectedListener {
-            when (it.itemId) {
-                R.id.bottom_navigation_archive -> {
-                    supportFragmentManager.beginTransaction()
-                        .replace(
-                            R.id.main_activity_frame,
-                            ArchiveFragment()
-                        ).commit()
-                    return@setOnNavigationItemSelectedListener true
-                }
-                R.id.bottom_navigation_notifications -> {
-                    supportFragmentManager.beginTransaction()
-                        .replace(R.id.main_activity_frame, NotificationsFragment()).commit()
-                    return@setOnNavigationItemSelectedListener true
-                }
-                R.id.bottom_navigation_home -> {
-                    supportFragmentManager.beginTransaction()
-                        .replace(
-                            R.id.main_activity_frame,
-                            HomeFragment()
-                        ).commit()
-                    return@setOnNavigationItemSelectedListener true
-                }
-                else -> {
-                    supportFragmentManager.beginTransaction()
-                        .replace(
-                            R.id.main_activity_frame,
-                            HomeFragment()
-                        ).commit()
-                    return@setOnNavigationItemSelectedListener true
-                }
-            }
-        }
+        val navController = findNavController(R.id.nav_host_fragment)
+        binding.navView.setupWithNavController(navController)
     }
 }
